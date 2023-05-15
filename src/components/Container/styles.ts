e67d191ea8e934917bee0e5e501bbe54e65f2c2b
Thiago <101950809+Azbito@users.noreply.github.com>
2023-05-15 16:36:01 -0300

    CHORE: New container

--- src/components/Container/styles.ts
+++ src/components/Container/styles.ts
@@ -10,3 +10,12 @@ export const Container = styled.div`
   margin-top: 2rem;
   gap: 2rem;
 `
+
+export const ContainerMap = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  align-items: center;
+  justify-content: center;
+  gap: 5rem;
+  margin-top: 2rem;
+`
