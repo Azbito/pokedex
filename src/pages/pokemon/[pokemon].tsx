bb884ca3f448b352ce59fe3c0fabe8cdb9c40fac
Thiago <101950809+Azbito@users.noreply.github.com>
2023-05-16 12:55:36 -0300

    CHORE: Pokemon infos

--- src/pages/pokemon/[pokemon].tsx
+++ src/pages/pokemon/[pokemon].tsx
@@ -1,11 +1,31 @@
+import { Container } from "@/components/Container/styles"
+import { DetailedCard } from "@/components/DetailedCard"
 import { PokeCard } from "@/components/PokeCard"
+import { CardTitle } from "@/components/PokeCard/styles"
+import { PokeProps } from "@/typings/pokeProps"
+import axios from "axios"
 import { useRouter } from "next/router"
 
-export default function PokemonPage() {
-  const router = useRouter()
-  const { pokemon } = router.query
+interface PokePageProps {
+  data: PokeProps
+}
 
-  return (<>
-    {pokemon}
-  </>)
+export default function PokemonPage({ data }: PokePageProps) {
+
+  return (
+    <Container>
+      <DetailedCard title={data.name} img={data.sprites.front_default} description={data.types[0].type.name} />
+    </Container>)
+}
+
+export async function getServerSideProps(context: any) {
+  const { pokemon } = context.params
+
+  const { data } = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
+
+  return {
+    props: {
+      data
+    }
+  }
 }
