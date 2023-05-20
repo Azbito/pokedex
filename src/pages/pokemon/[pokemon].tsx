4e98534a8b4c5c986dfcfb7097a14925d20ca156
Thiago <101950809+Azbito@users.noreply.github.com>
2023-05-19 22:25:03 -0300

    CHORE: DetaildCard's props applied

--- src/pages/pokemon/[pokemon].tsx
+++ src/pages/pokemon/[pokemon].tsx
@@ -1,7 +1,5 @@
 import { Container } from "@/components/Container/styles"
 import { DetailedCard } from "@/components/DetailedCard"
-import { PokeCard } from "@/components/PokeCard"
-import { CardTitle } from "@/components/PokeCard/styles"
 import { PokeProps } from "@/typings/pokeProps"
 import axios from "axios"
 import { useRouter } from "next/router"
@@ -12,12 +10,18 @@ interface PokePageProps {
 
 export default function PokemonPage({ data }: PokePageProps) {
 
-  console.log(data)
-
   return (
     <Container>
-      <DetailedCard title={data.name} img={data.sprites.front_default} description={data.types[0].type.name} height={data.height} weight={data.weight} />
-    </Container>)
+      <DetailedCard
+        name={data.name}
+        sprites={data?.sprites}
+        height={data.height}
+        weight={data.weight}
+        abilities={data.abilities}
+        types={data.types}
+      />
+    </Container>
+  )
 }
 
 export async function getServerSideProps(context: any) {
