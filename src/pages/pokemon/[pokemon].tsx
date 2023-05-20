d835257df37cd47de8a1826ac885515c84d3df95
Thiago <101950809+Azbito@users.noreply.github.com>
2023-05-20 18:58:01 -0300

    CHORE: getStaticPaths & getStaticProps

--- src/pages/pokemon/[pokemon].tsx
+++ src/pages/pokemon/[pokemon].tsx
@@ -1,7 +1,16 @@
-import { Container } from "@/components/Container/styles"
+//components
+import { BackButton } from "@/components/BackButton"
+import { BackButtonContainer, Container } from "@/components/Container/styles"
 import { DetailedCard } from "@/components/DetailedCard"
+
+//typings
 import { PokeProps } from "@/typings/pokeProps"
+
+//libs
 import axios from "axios"
+
+//hooks
+import { useRouter } from "next/router"
 
 interface PokePageProps {
   data: PokeProps
@@ -9,8 +18,13 @@ interface PokePageProps {
 
 export default function PokemonPage({ data }: PokePageProps) {
 
+  const router = useRouter()
+
   return (
     <Container>
+      <BackButtonContainer>
+        <BackButton onClick={() => router.back()} />
+      </BackButtonContainer>
       <DetailedCard
         name={data.name}
         sprites={data?.sprites}
@@ -23,14 +37,51 @@ export default function PokemonPage({ data }: PokePageProps) {
   )
 }
 
-export async function getServerSideProps(context: any) {
+export const getStaticPaths = async () => {
+  return {
+    paths: [
+      {
+        params: {
+          pokemon: 'pikachu'
+        }
+      },
+      {
+        params: {
+          pokemon: 'charmander'
+        }
+      },
+      {
+        params: {
+          pokemon: 'bulbassaur'
+        }
+      },
+      {
+        params: {
+          pokemon: 'mewtwo'
+        }
+      },
+    ],
+    fallback: true
+  }
+}
+
+
+export const getStaticProps = async (context: any) => {
   const { pokemon } = context.params
 
-  const { data } = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
+  try {
+    const { data } = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
 
-  return {
-    props: {
-      data
+    return {
+      props: {
+        data,
+        pokemon
+      }
+    }
+  } catch (error) {
+    alert(error)
+    return {
+      notFound: true
     }
   }
 }
